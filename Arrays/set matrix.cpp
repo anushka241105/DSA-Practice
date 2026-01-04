e152b8ad115bd2b574f24c28b8cdd7c73207248c
Anushka <anushka241105@gmail.com>
2026-01-04 23:54:58 +0530

    Medium

--- Arrays/set matrix.cpp
+++ Arrays/set matrix.cpp
@@ -1,5 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
+// BRUTE FORCE
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    for (int col = 0; col < n; col++) {
+                        if (matrix[i][col] != 0)
+                            matrix[i][col] = -1;
+                    }
+                    for (int row = 0; row < m; row++) {
+                        if (matrix[row][j] != 0)
+                            matrix[row][j] = -1;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == -1)
+                    matrix[i][j] = 0;
+            }
+        }
+    }
+};
+
+
+// BETTER APPROCH
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
@@ -25,3 +56,4 @@ public:
     // return matrix;
     }
 };
+// OPTIMAL APPROCH
