b1b294baf7483281824a8679aa52cd0ee78aa687
Anushka <anushka241105@gmail.com>
2026-01-13 21:56:09 +0530

    Medium

---
+++ Binary_search/find peak element.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+// BRUTE APPROCH
+class solution{
+public:
+    int findPeakElement(vector<int>& nums){
+    int n= nums.size();
+    for(int i=0; i<n; i++){
+        if((i==0 || nums[i-1]<nums[i]) && (i==n-1 || nums[i]>nums[i+1])){
+            return i;
+        }
+    }
+    return -1;
+    }
+};
