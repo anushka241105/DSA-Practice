5978942757721c42d110f234fbce32bfaca387e6
anushka <anushka241105@gmail.com>
2026-04-29 23:37:06 +0530

    STL

--- language_basics/standard template library.cpp
+++ language_basics/standard template library.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+
 //STL - provides predefined classes and functions
 // MAIN COMPONENTS:-
 //1> containers [sequence - vector, list, array, deque]
@@ -9,6 +11,20 @@ using namespace std;
 //3> Iterators [begin, end, rbegin, rbend]
 //4> Function objects
 
+
+void explainpair(){   //pair can also be a data type
+    
+    pair <int, int> p = {1,2};  
+    cout << p.first << p.second;
+    pair <int, pair<int,int>> a= {1,{2,3}};
+    cout<<a.first<<a.second.first<<a.second.second;
+    pair <int, int> arr[] = {{1,2}, {3,4}, {5,6}};   //lies inside the utility library
+    cout<<arr[1].second;
+}
+
+void explainvector(){
+
+}
 int main(){
 
 }
