b1241ecaeda58545841a08d1d62e59813de9e4ac
anushka <anushka241105@gmail.com>
2026-04-29 23:10:15 +0530

    STL

---
+++ language_basics/standard template library.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+//STL - provides predefined classes and functions
+// MAIN COMPONENTS:-
+//1> containers [sequence - vector, list, array, deque]
+            // [associative - set, map, multiset, multimap]
+            // [unordered - unordered_set, unordered_map] 
+//2> Algorithms [sort, reverese, find, count, binary_search]
+//3> Iterators [begin, end, rbegin, rbend]
+//4> Function objects
+
+int main(){
+
+}
